4ae297d9945bbada9c218f4d31e9d9335376f9ab
Skityaeva22 <skityaeva.anastasi@mail.ru>
2022-04-23 21:28:02 +0500

    Добавила сервлет выхода

---
+++ src/main/java/webCalc/Exit.java
@@ -0,0 +1,22 @@
+package webCalc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Exit
+ */
+@WebServlet("/Exit")
+public class Exit extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+        response.sendRedirect(request.getContextPath() + "/Login.jsp");
+	}
+}
