4572135c79074f1fb7b718e5e8f4e63f9ff6b5e7
Skityaeva22 <skityaeva.anastasi@mail.ru>
2022-04-23 21:30:39 +0500

    Заполнила сервлет авторизации

--- src/main/java/webCalc/Login.java
+++ src/main/java/webCalc/Login.java
@@ -1,5 +1,37 @@
 package webCalc;
 
-public class Login {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public Login() {
+        super();
+    }
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if(username.isEmpty() && password.isEmpty() )
+		{
+			request.setAttribute("err", "Поля пустые!");
+			request.getRequestDispatcher("Login.jsp").forward(request, response);
+		} else if (username.isEmpty()) {
+			request.setAttribute("err", "Введите имя пользователя!");
+			request.getRequestDispatcher("Login.jsp").forward(request, response);
+		} else if (password.isEmpty()) {
+			request.setAttribute("err", "Введите пароль!");
+			request.getRequestDispatcher("Login.jsp").forward(request, response);
+		} else
+		{
+		    response.sendRedirect("Calc");
+		}
+	}
 }
