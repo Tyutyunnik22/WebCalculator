5f8bce544d5953e4cabd78aaf3f17845c959e472
Salimgareev <kamilsalimgareev270@gmail.com>
2022-05-02 01:45:04 +0500

    Добавил обработку класса сервлета по открытию и скачиванию файла pdf

--- src/main/java/webCalc/DownloadFile.java
+++ src/main/java/webCalc/DownloadFile.java
@@ -1,5 +1,81 @@
 package webCalc;
 
-public class DownloadFile {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/DownloadFile")
+public class DownloadFile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public DownloadFile() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String key = request.getParameter("type");
+        
+		// reads input file from an absolute path
+        HttpSession session = request.getSession();
+        String jspPath = session.getServletContext().getRealPath("/");
+        String filePath = jspPath + "RepairCalculator.pdf";
+        File downloadFile = new File(filePath);
+        FileInputStream inStream = new FileInputStream(downloadFile);
+         
+        // if you want to use a relative path to context root:
+        String relativePath = getServletContext().getRealPath("");
+        System.out.println("relativePath = " + relativePath);
+         
+        // obtains ServletContext
+        ServletContext context = getServletContext();
+         
+        // gets MIME type of the file
+        String mimeType = context.getMimeType(filePath);
+        //String mimeType = context.getMimeType("application/pdf");
+        if (mimeType == null) {        
+            // set to binary type if MIME mapping not found
+            mimeType = "application/octet-stream";
+        }
+        System.out.println("MIME type: " + mimeType);
+         
+        // modifies response
+        response.setContentType(mimeType);
+        response.setContentLength((int) downloadFile.length());
+         
+        // forces download
+        if (key.equals("view")) {
+        	response.addHeader("Content-Disposition", "inline; filename=Documentation.pdf");
+        } else {
+        	String headerKey = "Content-Disposition";
+            String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+            response.setHeader(headerKey, headerValue);
+        }
+        
+        // obtains response's output stream
+        OutputStream outStream = response.getOutputStream();
+         
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+         
+        while ((bytesRead = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+         
+        inStream.close();
+        outStream.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 }
