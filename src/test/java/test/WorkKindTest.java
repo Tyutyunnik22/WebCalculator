47a133d753fd6108f75ff155b5451b565f187de1
Tyutyunnik22 <kolokol4ik07@gmail.com>
2022-05-03 19:27:40 +0500

    Добавила библиотеку в проект
    Создала тестовый класс, выполнила сборку проекта

---
+++ src/test/java/test/WorkKindTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WorkKindTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
