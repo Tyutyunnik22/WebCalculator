c6dd8ffb445b752738696284a2114f0645efda1f
Zarina <Zarina-Mamleeva@mail.ru>
2022-05-11 13:26:22 +0700

    Добавила документацию

--- src/main/java/webCalc/PdfResults.java
+++ src/main/java/webCalc/PdfResults.java
@@ -8,16 +8,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/** 
+ * Класс-сервлет обработки вывода окна результата создания pdf
+ * @author Mamleeva Z
+ * @version 1.0
+*/
 @WebServlet("/PdfResults")
 public class PdfResults extends HttpServlet {
+	/** Константа сериализации*/
 	private static final long serialVersionUID = 1L;
-    
+	/** Конструктор-стандартный*/
     public PdfResults() {
         super();
     }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    /**
+     * Метод обрабатыввает запросы получения данных
+     * @param request параметр запросов 
+     * @param response параметр ответов
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/PdfResults.jsp");
         dispatcher.forward(request, response);
 	}
