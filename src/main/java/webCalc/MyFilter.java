14ecfe2f32fa2a81c196ac827ee9914874bb5ffe
Tyutyunnik22 <kolokol4ik07@gmail.com>
2022-05-11 16:42:18 +0500

    Написала документацию.

--- src/main/java/webCalc/MyFilter.java
+++ src/main/java/webCalc/MyFilter.java
@@ -6,24 +6,41 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.*;
 
+/** 
+ * Класс для запрета перехода на jsp формы 
+ * @author Tyutyunnik E
+ * @version 1.0
+*/
 public class MyFilter implements Filter{
+	/**
+     * Метод определения конфигурационных параметров фильтра
+     * @param config объект конфигурации фильтра для передачи информации фильтру
+     */
 	public void  init(FilterConfig config) throws ServletException {
-		// Get init parameter 
+		// Получить параметр инициализации
 	}
+	/**
+     * Основной метод фильтрации
+     * @param request параметр запросов 
+     * @param response параметр ответов
+     * @param chain объект представления цепочки вызовов отфильтрованного запроса на ресурс
+     */
 	public void  doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
 		throws java.io.IOException, ServletException {
 		  
-		// Get the IP address of client machine.   
+		// Получить IP-адрес клиентской машины.   
 		String ipAddress = request.getRemoteAddr();
 		  
-		// Log the IP address and current timestamp.
+		// Зарегистрировать IP-адрес и текущую отметку времени.
 		System.out.println("IP "+ ipAddress + ", Time "+ new Date().toString());
 		  
 		HttpServletRequest req= (HttpServletRequest) request;
 		req.getRequestDispatcher("Login.jsp").forward(request,response);
 	}
+	/**
+     * Метод вызывается после того как фильтр заканчивает свою работу
+     */
 	public void destroy( ) {
-		/* Called before the Filter instance is removed 
-		from service by the web container*/
+		// Вызывается перед удалением экземпляра фильтра из службы веб-контейнера
 	}
 }
