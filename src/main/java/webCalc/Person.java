182253ef255f5e893c2eefb4f1559e65ec6a06dd
Zarina <Zarina-Mamleeva@mail.ru>
2022-05-11 13:42:34 +0700

    Добавила документацию

--- src/main/java/webCalc/Person.java
+++ src/main/java/webCalc/Person.java
@@ -1,6 +1,13 @@
 package webCalc;
 
+/** 
+* Класс-интерфейс субъекта
+* @author Mamleeva Z
+* @version 1.0
+*/
 public interface Person {
+	/**Метод возврата логина*/
 	String login();
+	/**Метод возврата логической переменой админа */
 	boolean isAdmin();
 }
